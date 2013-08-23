c9e29cd2defe7b47483d7bfb8475a873b19cb1e3
gavinblair <gavin.blair@rtraction.com>
2013-08-23 11:07:08 -0400

    bam

--- script.js
+++ script.js
@@ -2,23 +2,41 @@
     var gData;
     var ward;
   function gettheward(){
-    $.getJSON('http://opencouncil.ca/getward.php?a='+$('#address').val()+'&callback=gotward', function(data) {
+    if($("#address").val().length === 0){
+      $("#address").val("150 Dufferin Ave");
+    }
+    $.getJSON('http://opencouncil.ca/getward.php?a='+$('#address').val()+'&callback=?', null, function(data) {
       ward = data.ward;
-      $("#container").html(ich.results(data));
+
+      for(var i in gData){
+        if(gData[i].ward === ward.toString()){
+
+          data.info = gData[i];
+          data.notes = gData[i].notes;
+          if(data.info.vote === "destruct"){
+            data.verb = "tell them that you value our woodlands and wetlands";
+          } else {
+            data.verb = "thank them";
+          }
+          $("#container").html(ich.results(data));
+
+          break;
+        }
+      }
+
     });
-  }
-  function gotward(data){
-    console.log(data);
+
   }
   document.addEventListener('DOMContentLoaded', function() {
     $("#container").html(ich.getward);
     var URL = "0Ai6FEf_e95YqdFVSd1JwMWREZUdvRmxSc05mXzV5Y0E";
-    Tabletop.init( { key: URL, callback: showInfo, simpleSheet: true } ) 
-  }) 
+    Tabletop.init( { key: URL, callback: showInfo, simpleSheet: true } );
+    
+  });
     
   function showInfo(data) {
     // window.tabletopData = tabletop 
-    gData = data
-    console.log("here is gData", gData)
+    gData = data;
+    console.log("here is gData", gData);
 
   }
